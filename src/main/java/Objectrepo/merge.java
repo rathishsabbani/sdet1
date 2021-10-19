a03f63d2f4ef0c48d234afeb0bbf68e8ed542b1a
rathishsabbani <sabbani.rathish@gmail.com>
2021-10-19 15:30:36 +0530

    merge repo added now

---
+++ src/main/java/Objectrepo/merge.java
@@ -0,0 +1,9 @@
+package Objectrepo;
+
+public class merge {
+
+	public static void main(String[] args) {
+		System.out.println("output");
+	}
+
+}
--- src/test/java/com/Pom/tc_02contact.java
+++ src/test/java/com/Pom/tc_02contact.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import com.vtiger.generic.FileUtility;
-
 public class tc_02contact
 {
 	WebDriver driver;
